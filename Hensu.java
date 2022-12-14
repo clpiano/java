3fe09c2abebf0b1c16c8a86b3ef1b1714346ce82
clpiano <111512024+clpiano@users.noreply.github.com>
2022-12-14 19:17:25 +0900

    first commit

---
+++ Hensu.java
@@ -0,0 +1,10 @@
+//変数を使う際は、データ型 変数名 = "表示したい内容";
+//Javaを実行する際は、$ java ファイル名
+//クラス名はファイル名と同じ
+
+public class Hensu {
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        System.out.println(greeting);
+    }
+}
