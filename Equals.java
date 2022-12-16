8b8bbe6c19fc07e724980fcd1f3c0f1f153f96af
clpiano <111512024+clpiano@users.noreply.github.com>
2022-12-16 23:57:59 +0900

    study

--- Equals.java
+++ Equals.java
@@ -1,3 +1,5 @@
+//標準入力 equals
+
 import java.util.*;
 
 public class Equals {
@@ -6,11 +8,11 @@ public class Equals {
         String name = scan.next();
         System.out.println("Hello " + name);
 
-        if (name.equals("Java")) {
+        if (name.equals("Java")) {  //標準入力された文字列がJavaと一致すればwelcomeを表示
             System.out.println("Welcome");
-        } else if (name.equals("JAVA")) {
+        } else if (name.equals("JAVA")) {  //JAVAと一致すれば
             System.out.println("Good morning");
-        } else {
+        } else {  //上記以外
             System.out.println("Goodbye");
         }
     }
---
+++ For.java
@@ -0,0 +1,12 @@
+//繰り返し分 for
+
+
+public class For {
+  public static void main(String[] args) {
+      String greeting = "Hello paiza";
+
+      for (int i = 0; i < 5; i++) {  //5の部分は回数
+          System.out.println(greeting);
+      }
+  }
+}
