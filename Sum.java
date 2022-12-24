fd77c18856cf56bbb482becabd1cfcc1d1ceea65
clpiano <111512024+clpiano@users.noreply.github.com>
2022-12-25 00:32:39 +0900

    study

--- Sum.java
+++ Sum.java
@@ -6,12 +6,12 @@ import java.util.*;
 public class Sum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int count = scan.nextInt(); //繰り返す回数を標準入力
         int sum = 0;
-        for (int i = 0; i < 10; i++) { //10回繰り返し
+        for (int i = 0; i < count; i++) { //count回繰り返し
             sum += scan.nextInt();  //数値を入力し、sumメソッドに代入していく
         }
         System.out.println("合計値＝ " + sum);
-        System.out.println("平均値＝ " + (double)sum/10);
+        System.out.println("平均値＝ " + (double)sum/ count);  //doubleは小数点も含まれるデータ型
     }
 }
