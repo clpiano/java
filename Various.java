768ca5787e07886019f758410d205fe3e56f395a
clpiano <111512024+clpiano@users.noreply.github.com>
2022-12-26 00:20:51 +0900

    study

--- Various.java
+++ Various.java
@@ -1,19 +1,9 @@
 public class Various {
     public static void main(String[] args) {
-        String color = "black";
-
-        switch (color) {
-        case "red":
-            System.out.println("赤信号です");
-            break;
-        case "yellow":
-            System.out.println("黄信号です");
-            break;
-        case "blue":
-            System.out.println("青信号です");
-            break;
-        default:
-            System.out.println("信号の色ではありません");
+        for (int i = 1; i <= 10; i++) {
+            if (i%2 == 0) {
+              System.out.println(i);
+            }
         }
     }
 }
