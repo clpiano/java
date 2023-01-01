e6c46fa26809d057876d499a8c6ad143f65e6e32
clpiano <111512024+clpiano@users.noreply.github.com>
2023-01-01 13:14:10 +0900

    study

---
+++ Hairetsu2.java
@@ -0,0 +1,10 @@
+public class Hairetsu2 {
+    public static void main(String[] args) {
+        int arr[] = new int[] {12,34,45,21,33,4};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        System.out.println("Array Sum = "+sum);
+    }
+}
