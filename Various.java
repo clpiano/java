24562f47998694fde466b61479ea7d1712e6da15
clpiano <111512024+clpiano@users.noreply.github.com>
2023-01-02 23:26:24 +0900

    study

--- Various.java
+++ Various.java
@@ -1,7 +1,11 @@
 public class Various {
     public static void main(String[] args) {
         int [] number = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for (int i = 0; i < number.length; i++) {
+            sum += number[i];
+            System.out.println(sum);
+        }
 
-        System.out.println(number[0]);
     }
 }
