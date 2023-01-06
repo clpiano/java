ec5a545d1f1f609454e6f4eaae997e94aaa06b54
clpiano <111512024+clpiano@users.noreply.github.com>
2023-01-07 00:18:00 +0900

    study

--- Various.java
+++ Various.java
@@ -1,11 +1,21 @@
+//配列の要素に足し算
+
 public class Various {
     public static void main(String[] args) {
-        int [] number = {1,2,3,4,5,6,7,8,9,10};
-        int sum = 0;
-        for (int i = 0; i < number.length; i++) {
-            sum += number[i];
-            System.out.println(sum);
-        }
+        int [] number = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+        for (int i = 0; i < number[11]; i++) {
+            number[i] += 5;
+            if (number[i] % 2 != 0) {
 
+                System.out.println(number[i] + ",");
+
+            }
+        }
+        for (int i = 11; i < number.length; i++) {
+            number[i] += 5;
+            if(number[i] % 2 == 0) {
+                System.out.println(number[i] + ",");
+            }
+        }
     }
 }
